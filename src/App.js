f9b7b18a614d0ee0ab310e044f5a2b25c018d679
marie-rousselot <159030042+marie-rousselot@users.noreply.github.com>
2024-11-09 20:16:22 +0800

    added footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 
-import logo from "./codingpro_logo.png"
+import logo from "./codingpro_logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <footer className="App-footer">
+          <small>
+            Coded by ""
+            <a href="https://portfolio-project-marie-codingpro.netlify.app/">
+              Marie Rousselot
+            </a>
+          </small>
+          <main>
+            <Dictionary />
+          </main>
+        </footer>
+      </div>
     </div>
   );
 }
